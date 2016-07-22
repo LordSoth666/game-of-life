e116947ac025b115c9b082dfe6be0a6bf1c990de
Björn <bjoern.baumgarten81@gmx.de>
2016-07-22 10:13:23 +0200

    Added JavaDoc

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridWriter.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/GridWriter.java
@@ -1,9 +1,19 @@
 package com.wakaleo.gameoflife.domain;
 
+/**
+ * This class generates the Grid in which Game of life is running
+ * @author baumgabj
+ *
+ */
 public class GridWriter {
 
     private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
+    /**
+     * Converts a two dimensional Grid to String
+     * @param gridContents
+     * @return
+     */
     public String convertToString(final Cell[][] gridContents) {
         StringBuffer printedGrid = new StringBuffer();
         for (Cell[] row : gridContents) {
